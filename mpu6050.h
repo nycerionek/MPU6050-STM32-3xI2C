6029ec79b8f07ec6b5f9f2e45f2bc3c60204b998
nycerionek <warlinski@o2.pl>
2025-09-29 23:23:49 +0200

    Add files via upload

---
+++ mpu6050.h
@@ -0,0 +1,50 @@
+#ifndef INC_GY521_H_
+#define INC_GY521_H_
+
+#include <stdint.h>
+#include "i2c.h"
+
+#define MPU6050_ADDR_1 0xD0  // Adres pierwszego MPU6050
+#define MPU6050_ADDR_2 0xD0  // Adres drugiego MPU6050
+#define MPU6050_ADDR_3 0xD0  // Adres trzeciego MPU6050
+
+typedef struct
+{
+    int16_t Accel_X_RAW;
+    int16_t Accel_Y_RAW;
+    int16_t Accel_Z_RAW;
+    double Ax;
+    double Ay;
+    double Az;
+
+    int16_t Gyro_X_RAW;
+    int16_t Gyro_Y_RAW;
+    int16_t Gyro_Z_RAW;
+    double Gx;
+    double Gy;
+    double Gz;
+
+    float Temperature;
+
+    double KalmanAngleX;
+    double KalmanAngleY;
+} MPU6050_t;
+
+typedef struct
+{
+    double Q_angle;
+    double Q_bias;
+    double R_measure;
+    double angle;
+    double bias;
+    double P[2][2];
+} Kalman_t;
+
+uint8_t MPU6050_Init(I2C_HandleTypeDef *I2Cx, uint8_t addr);
+void MPU6050_Read_Accel(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct, uint8_t addr);
+void MPU6050_Read_Gyro(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct, uint8_t addr);
+void MPU6050_Read_Temp(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct, uint8_t addr);
+void MPU6050_Read_All(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct, uint8_t addr);
+double Kalman_getAngle(Kalman_t *Kalman, double newAngle, double newRate, double dt);
+
+#endif /* INC_GY521_H_ */
